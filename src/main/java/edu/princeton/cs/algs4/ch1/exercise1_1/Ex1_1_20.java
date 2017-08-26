9753a6eff0dec7ae3c1bb4a22a95e8d82f07ecca
zhang_yan <rockflyrain@gmail.com>
2017-08-27 00:56:29 +0800

    update

--- src/main/java/edu/princeton/cs/algs4/ch1/exercise1_1/Ex1_1_20.java
+++ src/main/java/edu/princeton/cs/algs4/ch1/exercise1_1/Ex1_1_20.java
@@ -1,11 +1,18 @@
 package edu.princeton.cs.algs4.ch1.exercise1_1;
+
 /**
  * Created by yan.zhang on 8-21.
  */
 public class Ex1_1_20 {
 
     public static void main(String[] args) {
-        
+        System.out.println(getLn(10));
     }
 
+    public static double getLn(double n) {
+        if (n == 1D) {
+            return 0D;
+        }
+        return Math.log(n) + getLn(n - 1);
+    }
 }
---
+++ src/test/java/edu/princeton/cs/algs4/ch1/exercise1_1/DrawDemoTest.java
@@ -0,0 +1,11 @@
+package edu.princeton.cs.algs4.ch1.exercise1_1;
+/**
+ * Created by yan.zhang on 8-26.
+ */
+public class DrawDemoTest {
+
+    //@Test
+    //public void drawLine() throws Exception {
+    //    StdDraw.line(1, 1, 2, 2);
+    //}
+}
